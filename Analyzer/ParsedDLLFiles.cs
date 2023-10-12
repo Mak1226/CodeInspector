b17404fb2dd911fa00bfa96eea5a0f43b3d932d0
Lakshmi Nikhitha Atyam <nikhithaatyam@gmail.com>
2023-10-13 00:07:50 +0530

    parsing of dll files is started

--- Analyzer/ParsedDLLFiles.cs
+++ Analyzer/ParsedDLLFiles.cs
@@ -7,16 +7,50 @@ using System.Threading.Tasks;
 
 namespace Analyzer
 {
-    internal class ParsedDLLFiles
+    public class ParsedDLLFiles
     {
-
-        public List<ParsedNamespace>? _namespaces;
-
-        ParsedDLLFiles(List<string> paths) // path of dll files
+        public List<ParsedClass> classObjList = new();
+        public List<ParsedInterface> interfaceObjList = new();
+        public ParsedDLLFiles(List<string> paths) // path of dll files
         {
-            
             // take the input of all the dll files
             // it merge the all the ParsedNamespace
+            foreach(var path in paths) 
+            {
+                Assembly assembly = Assembly.LoadFrom(path);
+
+                if (assembly != null)
+                {
+                    Type[] types = assembly.GetTypes();
+
+                    foreach (Type type in types)
+                    {
+                        if (type.Namespace != null)
+                        {
+                            if (type.Namespace.StartsWith("System.") || type.Namespace.StartsWith("Microsoft."))
+                            {
+                                continue;
+                            }
+
+                            if (type.IsClass)
+                            {
+                                ParsedClass classObj = new ParsedClass(type);
+                                classObjList.Add(classObj);
+                            }
+                            else if (type.IsInterface)
+                            {
+                                ParsedInterface interfaceObj = new ParsedInterface(type);
+                                interfaceObjList.Add(interfaceObj);
+                            }
+                        }
+                        else
+                        {
+                            // code written outside all namespaces may have namespace as null
+                            // will handle later
+                        }
+                    }
+                }
+            }
 
         }
 
