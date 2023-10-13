7e66c9c48b32fcb27824c9f24327a7aa6bb65aee
saarang <saarangs2002@gmail.com>
2023-10-13 16:39:10 +0530

    Dashboard UI

---
+++ Dashboard/Dashboard/StudentView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard
+{
+    /// <summary>
+    /// Interaction logic for StudentView.xaml
+    /// </summary>
+    public partial class StudentView : Page
+    {
+        public StudentView()
+        {
+            InitializeComponent();
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.NavigationService != null)
+            {
+                this.NavigationService.Navigate(new Uri("Login.xaml", UriKind.Relative));
+            }
+        }
+    }
+}
