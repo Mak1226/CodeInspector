9be4f063e82b32f28b3f2f0b7d0baf946fd88cc6
unniisme <unniisme@gmail.com>
2023-10-18 23:11:02 +0530

    Created unit testing module

---
+++ ContentUnitTesting/FileEncoderUnitTests.cs
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Filename     = FileEncoderUnitTests.cs
+ * 
+ * Author       = Susan
+ *
+ * Product      = Analyzer
+ * 
+ * Project      = ContentUnitTesting
+ *
+ * Description  = Unit tests for IFileEncoder
+*****************************************************************************/
+using Content;
+
+namespace ContentUnitTesting
+{
+    [TestClass]
+    public class FileEncoderUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
