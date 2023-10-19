6c55f8389cc1bd3f6a19a6760804693e3943b2c5
padath314 <112001057@smail.iitpkd.ac.in>
2023-10-19 19:20:04 +0530

    UnitTests working

--- ContentUnitTesting/FileEncoderUnitTests.cs
+++ ContentUnitTesting/FileEncoderUnitTests.cs
@@ -9,6 +9,8 @@
  *
  * Description  = Unit tests for IFileEncoder
 *****************************************************************************/
+using System.Diagnostics;
+using System.Text;
 using Content;
 
 namespace ContentUnitTesting
@@ -16,15 +18,28 @@ namespace ContentUnitTesting
     [TestClass]
     public class FileEncoderUnitTests
     {
+        private string _testDirectory;
+
+        [TestInitialize]
+        public void TestInitialize() 
+        {
+            _testDirectory = Directory.GetParent( Environment.CurrentDirectory ).Parent.Parent.FullName;
+        }
+
         [TestMethod]
         public void GetEncoded_ReturnsValidXMLString()
         {
-            // Arrange
-            var encoder = new DLLEncoder();
-            var filePaths = new List<string>
+            
+            // Define the list of test file names (assuming they are already in the TestDlls directory)
+            var testFileNames = new List<string>
             {
-                "TestDlls/HelloWorld.dll"
+                "HelloWorld.dll"
             };
+
+            var encoder = new DLLEncoder();
+
+            // Create a list of file paths based on the files you've copied or created
+            var filePaths = testFileNames.Select( fileName => Path.Combine( _testDirectory , fileName ) ).ToList();
 
             // Act
             string encodedXML = encoder.GetEncoded( filePaths );
@@ -33,5 +48,50 @@ namespace ContentUnitTesting
             Assert.IsFalse( string.IsNullOrEmpty( encodedXML ) );
         }
 
+        [TestMethod]
+        public void EncodeAndDecodeFiles_CheckEquality()
+        {
+            // Arrange
+            DLLEncoder encoder = new();
+
+            // Test DLL files
+            var testFileNames = new List<string>
+            {
+                "Testdlls/HelloWorld.dll"
+            };
+
+            var filePaths = testFileNames.Select( fileName => Path.Combine( _testDirectory , fileName ) ).ToList();
+
+            // Save the file paths and content into a dictionary before encoding
+            Dictionary<string , string> dataBeforeEncoding = new();
+            foreach (string filePath in filePaths)
+            {
+                Trace.WriteLine( filePath );
+                //Assert.IsTrue( false , filePath );
+                string content = File.ReadAllText( filePath, Encoding.UTF8 );
+                dataBeforeEncoding[filePath] = content;
+            }
+
+            // Act
+            string encodedXML = encoder.GetEncoded( filePaths );
+
+            Assert.IsFalse( string.IsNullOrEmpty( encodedXML ),
+                "Encoded XML is empty");
+
+            // Decode the XML back to file paths
+            encoder.DecodeFrom( encodedXML );
+            Dictionary<string , string> decodedData = encoder.GetData();
+
+            // Assert
+            CollectionAssert.AreEqual( dataBeforeEncoding.Keys , decodedData.Keys );
+            foreach (string filePath in filePaths)
+            {
+                Assert.IsTrue( decodedData.ContainsKey( filePath ) );
+                Assert.AreEqual( dataBeforeEncoding[filePath] , decodedData[filePath] );
+            }
+
+        }
+
     }
+    
 }
