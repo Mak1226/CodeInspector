05b6d9e891f70b8129cbe89f0c26a96a361619d6
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-10-20 09:30:51 +0530

    Added comments, communicationFactory

--- Networking/Communicator/Client.cs
+++ Networking/Communicator/Client.cs
@@ -1,26 +1,30 @@
-﻿using System;
+﻿/////
+/// Author: 
+/////
+
+using System;
 using System.Diagnostics;
 
 namespace Networking.Communicator
 {
-    public class Communicator : ICommunicator
+    public class Client : ICommunicator
     {
         void ICommunicator.Send(string serializedObj, string eventType, string? destID)
         {
-            Trace.WriteLine("Send" + serializedObj + " " + eventType + " " + destID);
+            Trace.WriteLine("[Client] Send" + serializedObj + " " + eventType + " " + destID);
             //throw new NotImplementedException();
         }
 
         string ICommunicator.Start(string? destIP, string? destPort)
         {
-            Trace.WriteLine("Start" + destIP + " " + destPort);
+            Trace.WriteLine("[Client] Start" + destIP + " " + destPort);
             return "";
             //throw new NotImplementedException();
         }
 
         void ICommunicator.Stop()
         {
-            Trace.WriteLine("Stop");
+            Trace.WriteLine("[Client] Stop");
             //throw new NotImplementedException();
         }
     }
--- Networking/Communicator/ICommunicator.cs
+++ Networking/Communicator/ICommunicator.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/////
+/// Author: 
+/////
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +12,29 @@ namespace Networking.Communicator
 {
     public interface ICommunicator
     {
+        /// <summary>
+        /// start(server dest) -> client:
+        ///     call the listen start all threads connect to server 
+        /// start(server dest=null) -> server:
+        ///     call the listen start all threads returns serverID
+        /// </summary>
+        /// <param name="destIP">Server IP when called by client</param>
+        /// <param name="destPort">Server Port when called by client</param>
+        /// <returns>server: Server IP, port</returns>
         public string Start(string? destIP =null, string? destPort=null);
+
+        /// <summary>
+        /// Server: Stops the server and stops all threads
+        /// Client: Stops listening to the server and stops all threads
+        /// </summary>
         public void Stop();
+
+        /// <summary>
+        /// Sends `serializedObj` to `destID`. We call `Send` upon happening of event `eventType`.
+        /// </summary>
+        /// <param name="serializedObj"></param>
+        /// <param name="eventType"></param>
+        /// <param name="destID"></param>
         //public void Send(object obj,string? destID=null);
         public void Send(string serializedObj, string eventType/* will be updated with a class obj (?) */, string? destID=null);
         
