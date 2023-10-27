67b38666bd6e42bf27abf466d7aa0988e7a8b629
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-10-27 22:34:42 +0530

    updated

--- Networking/IEventHandler.cs
+++ Networking/IEventHandler.cs
@@ -2,18 +2,15 @@
 /// Author: 
 /////
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Networking
 {
     public interface IEventHandler
     {
-        public String handleFile();
-        public String handleChatMessage();
-        public String handleAnalyserResult();
+        public string handleFile();
+        public string handleChatMessage();
+        public string handleAnalyserResult();
+        public string connectionRequest();
+        public string clientJoined();
+        public string clientLeft();
     }
 }
---
+++ Networking/Queues/GenericQueue.cs
@@ -0,0 +1,50 @@
+﻿/////
+/// Author: 
+/////
+
+using System;
+using System.Diagnostics;
+
+namespace Networking.Queues
+{
+	public class GenericQueue
+	{
+		private Queue<string> _queue;
+
+		public GenericQueue()
+		{
+			this._queue = new();
+		}
+
+		public bool Enqueue(string data)
+		{
+			try
+			{
+				this._queue.Enqueue(data);
+                return true;
+            }
+			catch (Exception ex)
+			{
+				Trace.WriteLine("[Queue] Exception found during enqueue");
+                Trace.WriteLine($"{ex.StackTrace}");
+            }
+			return false;
+        }
+
+        public string Dequeue()
+        {
+            try
+            {
+                while (_queue.Count == 0) ;
+                return _queue.Dequeue();
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("[Queue] Exception found");
+                Trace.WriteLine($"{ex.StackTrace}");
+            }
+            return "";
+        }
+    }
+}
+
