94ae6c55aec71488e9194da9ff5531739ec4c57f
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-11-01 22:17:37 +0530

    started networkstream

--- Networking/Communicator/Server.cs
+++ Networking/Communicator/Server.cs
@@ -4,14 +4,38 @@
 
 using System;
 using System.Diagnostics;
+using System.Text.Json; 
+using System.Net.Sockets;
 
 namespace Networking.Communicator
 {
+    public class Message
+    {
+        public string _serializedObj { get; }
+        public string _eventType { get; }
+        public Message(string serializedObj, string eventType)
+        {
+            _serializedObj = serializedObj;
+            _serializedObj = eventType;
+        }
+    }
     public class Server : ICommunicator
     {
+        Dictionary<string, NetworkStream>? _clientIDToStream;
+        private byte[] returnBytes(string serializedObj, string eventType)
+        {
+            var data = new Message(serializedObj, eventType);
+            string serStr = JsonSerializer.Serialize(data);
+            return System.Text.Encoding.ASCII.GetBytes(serStr);
+        }
         void ICommunicator.Send(string serializedObj, string eventType, string? destID)
         {
             Trace.WriteLine("[Server] Send" + serializedObj + " " + eventType + " " + destID);
+            if(destID!=null)
+            {
+                byte[] message = returnBytes(serializedObj, eventType);
+                _clientIDToStream[destID].Write(message);
+            }
             //throw new NotImplementedException();
         }
 
