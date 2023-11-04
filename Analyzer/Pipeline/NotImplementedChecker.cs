65f167785c84b8a2fed3e592062621a5e147d3df
Kaustubh Sapkale <112001016@smail.iitpkd.ac.in>
2023-11-04 12:36:25 +0530

    added notimplemetedmethodchecker and number of IL code lines

---
+++ Analyzer/Pipeline/NotImplementedChecker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analyzer.Parsing;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace Analyzer.Pipeline.Analyzers
+{
+    internal class NotImplementedChecker:BaseAnalyzer
+    {
+        public NotImplementedChecker(ParsedDLLFiles dllFiles) : base(dllFiles)
+        {
+            // The constructor can be used for any necessary setup or initialization.
+        }
+        public int CountUnimplementedMethods(ParsedDLLFiles dllFiles)
+        {
+            int unimplementedMethodCount = 0;
+
+            foreach (var classObjMC in dllFiles.classObjListMC)
+            {
+                foreach (var method in classObjMC.Methods)
+                {
+                    if (!IsImplemented((MethodDefinition)method))
+                    {
+                        unimplementedMethodCount++;
+                    }
+                }
+            }
+
+            return unimplementedMethodCount;
+        }
+
+        private static bool IsImplemented(MethodDefinition method)
+        {
+            if (method.HasBody)
+            {
+                foreach (Instruction instruction in method.Body.Instructions)
+                {
+                    if (instruction.OpCode == OpCodes.Newobj)
+                    {
+                        MethodReference constructor = (MethodReference)instruction.Operand;
+                        if (constructor.DeclaringType.FullName == "System.NotImplementedException")
+                        {
+                            return false; // Unimplemented method
+                        }
+                    }
+                }
+            }
+
+            return true; // Implemented method
+        }
+    }
+}
---
+++ Analyzer/Pipeline/NumberOfLines.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Mono.Cecil;
+using System.Text.RegularExpressions;
+using Analyzer.Parsing;
+using System.Reflection;
+
+namespace Analyzer.Pipeline.Analyzers
+{
+    public class NumberOfLines : BaseAnalyzer
+    {
+        public NumberOfLines(ParsedDLLFiles dllFiles) : base(dllFiles)
+        {
+            // The constructor can be used for any necessary setup or initialization.
+        }
+        public static int CountFunctionLength(ParsedDLLFiles dllFiles)
+        {
+            int functionLength = 0;
+
+            foreach (var classObjMC in dllFiles.classObjListMC)
+            {
+                foreach (var method in classObjMC.Methods)
+                {
+                    // Count the number of lines in the method's IL code
+                    int ilLineCount = CountILLinesInMethod((MethodDefinition)method);
+
+                    functionLength += ilLineCount;
+                }
+            }
+
+            return functionLength;
+        }
+
+        private static int CountILLinesInMethod(MethodDefinition method)
+        {
+            if (method.Body != null)
+            {
+                // Count the IL instructions in the method body
+                int ilLineCount = method.Body.Instructions.Count;
+
+                return ilLineCount;
+            }
+
+            return 0;
+        }
+    }
+}
