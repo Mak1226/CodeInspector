96934a1648545774d80084ffe8737655bc6fbe61
shubhang kedia <112001040@smail.iitpkd.ac.in>
2023-11-04 15:49:10 +0530

    code is working

---
+++ ServerApp/ServerApp.cs
@@ -0,0 +1,16 @@
+﻿using Networking.Communicator; // Assuming this is the namespace of your class library
+using System;
+
+namespace ServerApp
+{
+    class ServerApp
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.Start(null, null);
+            Console.ReadKey();
+            server.Send("omg", "1123", "A");
+        }
+    }
+}
