aac3c19a504f871cb88b063beb5cd0eb75c9550b
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-11-05 19:11:43 +0530

    clientapp using commfactory

--- ClientApp/ClientApp.cs
+++ ClientApp/ClientApp.cs
@@ -9,14 +9,15 @@ namespace ClientApp
         public static void Main()
         {
             Console.WriteLine("New client making");
-            var client = new Client();
+            //var client = new Client();
+            ICommunicator client = CommunicationFactory.GetCommunicator(false);
             Console.WriteLine("Starting client");
-            client.Start("0.0.0.0", 12345);
+            client.Start("192.168.0.108", 12345);
             Console.WriteLine("client started");
-            //client.Send("123.456.789.101", "someEvent", "23");
-            //Console.WriteLine("client sent");
             IEventHandler ev = new Events();
             client.Subscribe(ev, "someModule");
+            client.Send("123.456.789.101", "someEvent", "server");
+            Console.WriteLine("client sent");
         }
 	}
 }
