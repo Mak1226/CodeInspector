4a0ef10e281df225086108d23941d4b77df12d23
unniisme <unniisme@gmail.com>
2023-11-07 22:44:51 +0530

    Added a server

---
+++ Content/Server/ServerRecieveHandler.cs
@@ -0,0 +1,45 @@
+﻿using Networking;
+using Networking.Utils;
+
+namespace Content.Server
+{
+    internal class ServerRecieveHandler : IEventHandler
+    {
+        ContentServer _server;
+
+        public ServerRecieveHandler(ContentServer server)
+        {
+            _server = server;
+        }
+        public string HandleAnalyserResult(Message data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HandleChatMessage(Message data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HandleClientJoined(Message data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HandleClientLeft(Message data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HandleConnectionRequest(Message data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HandleFile(Message data)
+        {
+            _server.HandleRecieve(data.SerializedObj, data.SenderID);
+            return "";
+        }
+    }
+}
