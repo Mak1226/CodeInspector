e71b434d988842aa82e0abd4c740910d67ae7c85
Sneha <253670@ust.com>
2023-11-10 06:41:51 +0530

    Not completed yet

--- Analyzer/Pipeline/NativeFieldsShouldNotBeVisible.cs
+++ Analyzer/Pipeline/NativeFieldsShouldNotBeVisible.cs
@@ -1,64 +1,69 @@
-﻿using Analyzer.Parsing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
+﻿//using Analyzer.Parsing;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Mono.Cecil;
+//using Mono.Cecil.Cil;
 
-namespace Analyzer.Pipeline
-{
-    internal class NativeFieldsShouldNotBeVisible : BaseAnalyzer
-    {
-        public NativeFieldsShouldNotBeVisible(ParsedDLLFiles dllFiles) : base(dllFiles)
-        {
-            // TODO if required
-        }
+//namespace Analyzer.Pipeline
+//{
+//    internal class NativeFieldsShouldNotBeVisible : AnalyzerBase
+//    {
+//        public NativeFieldsShouldNotBeVisible(ParsedDLLFiles dllFiles) : base(dllFiles)
+//        {
+//            // TODO if required
+//        }
 
-        public List<Type> FindVisibleNativeFields()
-        {
-            List<Type> visibleNativeFieldsList = new List<Type>();
+//        public List<Type> FindVisibleNativeFields()
+//        {
+//            List<Type> visibleNativeFieldsList = new List<Type>();
 
-            foreach (ParsedClassMonoCecil classObj in parsedDLLFiles.classObjListMC)
-            {
-                TypeDefinition classType = classObj.TypeObj;
-                // rule does not apply to interface, enumerations and delegates or to types without fields
-                if (!classType.IsValueType)
-                {
-                    // TODO
-                    // For now implementing just for classes
-                    // Classes and namespaces can have structures
-                    // Structures are recommended to be immutable too
-                    // Otherwise they should also have getter and setter
-                    // NOTE: parsedDLLFiles.classObjListMC provides only classes
-                    continue;
-                }
+//            foreach (ParsedClassMonoCecil classObj in parsedDLLFiles.classObjListMC)
+//            {
+//                TypeDefinition classType = classObj.TypeObj;
+//                // rule does not apply to interface, enumerations and delegates or to types without fields
+//                if (!classType.IsValueType)
+//                {
+//                    // TODO
+//                    // For now implementing just for classes
+//                    // Classes and namespaces can have structures
+//                    // Structures are recommended to be immutable too
+//                    // Otherwise they should also have getter and setter
+//                    // NOTE: parsedDLLFiles.classObjListMC provides only classes
+//                    continue;
+//                }
 
-                if (!classType.HasFields)
-                {
-                    continue;
-                }
+//                if (!classType.HasFields)
+//                {
+//                    continue;
+//                }
 
-                foreach (FieldDefinition field in classType.Fields)
-                {
-                    if (!field.IsVisible())
-                    {
-                        continue;
-                    }
+//                foreach (FieldDefinition field in classType.Fields)
+//                {
+//                    if (!field.IsVisible())
+//                    {
+//                        continue;
+//                    }
 
-                    //not readonly native fields or arrays of native fields
-                    if ((field.FieldType.IsNative() && !field.IsInitOnly) ||
-                        (field.FieldType.IsArray && field.FieldType.GetElementType().IsNative()))
-                    {
+//                    //not readonly native fields or arrays of native fields
+//                    if ((field.FieldType.IsNative() && !field.IsInitOnly) ||
+//                        (field.FieldType.IsArray && field.FieldType.GetElementType().IsNative()))
+//                    {
 
-                        //
-                    }
-                }
-            }
+//                        //
+//                    }
+//                }
+//            }
 
-            return visibleNativeFieldsList;
+//            return visibleNativeFieldsList;
 
-        }
-    }
-}
+//        }
+
+//        public override AnalyzerResult Run()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
--- Analyzer/Pipeline/NoHardCodedPaths.cs
+++ Analyzer/Pipeline/NoHardCodedPaths.cs
@@ -1,65 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Analyzer.Parsing;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Analyzer.Parsing;
+//using Mono.Cecil;
+//using Mono.Cecil.Cil;
 
-namespace Analyzer.Pipeline
-{
-    internal class NoHardCodedPaths : BaseAnalyzer
-    {
-        // (back)slash counters
-        private int slashes;
-        private int backslashes;
+//namespace Analyzer.Pipeline
+//{
+//    internal class NoHardCodedPaths : BaseAnalyzer
+//    {
+//        // (back)slash counters
+//        private int slashes;
+//        private int backslashes;
 
-        // method body being checked
-        private MethodBody method_body;
+//        // method body being checked
+//        private MethodBody method_body;
 
-        // point counter
-        private int current_score;
+//        // point counter
+//        private int current_score;
 
-        private static Dictionary<string, bool> resultCache = new Dictionary<string, bool>();
+//        private static Dictionary<string, bool> resultCache = new Dictionary<string, bool>();
 
-        public NoHardCodedPaths(ParsedDLLFiles dllFiles) : base(dllFiles)
-        {
-            // TODO if required
-        }
-        void AddPoints(int pts)
-        {
-            current_score += pts;
-        }
+//        public NoHardCodedPaths(ParsedDLLFiles dllFiles) : base(dllFiles)
+//        {
+//            // TODO if required
+//        }
+//        void AddPoints(int pts)
+//        {
+//            current_score += pts;
+//        }
 
-        public Dictionary<string, bool> FindHardCodedPaths()
-        {
-            return resultCache;
-        }
+//        public Dictionary<string, bool> FindHardCodedPaths()
+//        {
+//            return resultCache;
+//        }
 
-        static bool CanBeWindowsAbsolutePath(string s)
-        {
-            // true for strings like ?:\*
-            // e.g. 'C:\some\path' or 'D:\something.else"
-            return s[1] == ':' && s[2] == '\\';
-        }
+//        static bool CanBeWindowsAbsolutePath(string s)
+//        {
+//            // true for strings like ?:\*
+//            // e.g. 'C:\some\path' or 'D:\something.else"
+//            return s[1] == ':' && s[2] == '\\';
+//        }
 
-        static bool CanBeWindowsUNCPath(string s)
-        {
-            // true for Windows UNC paths
-            // e.g. \\Server\Directory\File
-            return s[0] == '\\' && s[1] == '\\';
-        }
+//        static bool CanBeWindowsUNCPath(string s)
+//        {
+//            // true for Windows UNC paths
+//            // e.g. \\Server\Directory\File
+//            return s[0] == '\\' && s[1] == '\\';
+//        }
 
-        static bool CanBeUnixAbsolutePath(string s)
-        {
-            // true for strings like /*
-            return s[0] == '/';
-        }
+//        static bool CanBeUnixAbsolutePath(string s)
+//        {
+//            // true for strings like /*
+//            return s[0] == '/';
+//        }
 
-        public override int GetScore()
-        {
-            return 0;
-        }
-    }
-}
+//        public override int GetScore()
+//        {
+//            return 0;
+//        }
+//    }
+//}
