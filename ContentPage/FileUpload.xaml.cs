8aad5a45cd7abdab5329fbbae00140e81b97447b
unniisme <unniisme@gmail.com>
2023-11-17 00:51:29 +0530

    Clean Up

--- ContentPage/FileUpload.xaml.cs
+++ ContentPage/FileUpload.xaml.cs
@@ -11,13 +11,10 @@
  *****************************************************************************/
 
 using Content.ViewModel;
-using Networking.Communicator;
 using System.Diagnostics;
-using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
-using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace ContentPage
 {
@@ -40,41 +37,12 @@ namespace ContentPage
             InitializeComponent();
         }
 
-        //private void UploadButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
-        //    bool? response = openFileDialog.ShowDialog();
-        //    if (response == true)
-        //    {
-        //        IFileHandler upload_file = new FileHandler();
-        //        string filepath = openFileDialog.FileName;
-        //        Trace.WriteLine(filepath);
-        //        //upload_file.Upload(filepath,"5");
-        //    }
-
-        //}
-
-
 
         /// <summary>
         /// Handles a directory being provided to be uploaded
         /// </summary>
         /// <param name="sender">Sender</param>
         /// <param name="e">Event args</param>
-        //private void UploadButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    System.Windows.Forms.FolderBrowserDialog folderDialog = new();
-        //    System.Windows.Forms.DialogResult result = folderDialog.ShowDialog();
-
-        //    if (result == System.Windows.Forms.DialogResult.OK)
-        //    {
-        //        string folderPath = folderDialog.SelectedPath;
-        //        Trace.WriteLine(folderPath);
-
-        //        // Pass folder path to Content Client
-        //        _uploadClient.HandleUpload(folderPath);
-        //    }
-        //}
         private void UploadButton_Click(object sender, RoutedEventArgs e)
         {
             //Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
