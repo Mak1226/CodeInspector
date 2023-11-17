449b1e11ca40cb4e126ebf5a9809992d2a4022cf
thanmayee07 <112001013@smail.iitpkd.ac.in>
2023-11-17 09:56:25 +0530

    added tests

---
+++ AnalyzerTests/Pipeline/TestAvoidGotoStatements.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Analyzer.Pipeline;
+using System.Collections.Generic;
+using Analyzer.Parsing;
+using System.Linq;
+
+namespace Analyzer.Pipeline.Tests
+{
+    [TestClass]
+    public class AvoidGotoStatementsAnalyzerTests
+    {
+        [TestMethod]
+        public void GotoStatementsPresent_ShouldFail()
+        {
+            List<string> dllFilePaths = new List<string> { "..\\..\\..\\..\\Analyzer\\TestDLLs\\Goto.dll" };
+
+            List<ParsedDLLFile> dllFiles = dllFilePaths.Select(path => new ParsedDLLFile(path)).ToList();
+            AvoidGotoStatementsAnalyzer analyzer = new AvoidGotoStatementsAnalyzer(dllFiles);
+
+            var result = analyzer.AnalyzeAllDLLs();
+
+            Assert.AreEqual(0, result[dllFiles[0].DLLFileName].Verdict); // Verdict should be 0 for failing
+        }
+    }
+}
