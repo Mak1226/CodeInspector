3bc62396694755d518fba95a96123451941a51bb
whiteGhost-08 <08absahil@gmail.com>
2023-11-17 08:43:19 +0530

    added cloud and unit tests

---
+++ UnitTests/SessionAndAnalysisTests.cs
@@ -0,0 +1,96 @@
+﻿using ServerlessFunc;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass()]
+    public class SessionAndAnalysisTests
+    {
+        private string analysisUrl = "http://localhost:7074/api/analysis";
+        private string submissionUrl = "http://localhost:7074/api/submission";
+        private string sessionUrl = "http://localhost:7074/api/session";
+        private DownloadApi _downloadClient;
+        private UploadApi _uploadClient;
+
+        public SessionAndAnalysisTests()
+        {
+            _downloadClient = new DownloadApi(sessionUrl, submissionUrl, analysisUrl);
+            _uploadClient = new UploadApi(sessionUrl, submissionUrl, analysisUrl);
+        }
+
+        public SessionData GetDummySessionData()
+        {
+            SessionData sessionData = new SessionData();
+            sessionData.HostUserName = "name1";
+            sessionData.SessionId = "1";
+            List<string> Test = new List<string>
+            {
+                "101",
+                "102"
+            };
+            sessionData.Tests = InsightsUtility.ListToByte(Test);
+            List<string> Student = new List<string>
+            {
+                "Student1",
+                "Student2"
+            };
+            sessionData.Students = InsightsUtility.ListToByte(Student);
+            List<Tuple<string, string>> NameToID = new List<Tuple<string, string>>
+            {
+                Tuple.Create("Test1", "101"),
+                Tuple.Create("Test2", "102")
+            };
+            sessionData.TestNameToID = InsightsUtility.ListTupleToByte(NameToID);
+            return sessionData;
+        }
+
+        [TestMethod()]
+
+        public async Task PostAndGetTestSession()
+        {
+            await _downloadClient.DeleteAllSessionsAsync();
+            SessionData sessionData = GetDummySessionData();
+            await _uploadClient.PostSessionAsync(sessionData);
+            IReadOnlyList<SessionEntity> sessionEntity = await _downloadClient.GetSessionsByHostNameAsync("name1");
+            await _downloadClient.DeleteAllSessionsAsync();
+            Assert.AreEqual(1, sessionEntity.Count);
+            CollectionAssert.AreEqual(sessionData.Students, sessionEntity[0].Students, "Students list mismatch");
+            CollectionAssert.AreEqual(sessionData.Tests, sessionEntity[0].Tests, "Tests list mismatch");
+
+        }
+        [TestMethod()]
+        public async Task PostAndGetTestSubmission()
+        {
+
+            SubmissionData submission = new SubmissionData();
+            submission.SessionId = "1";
+            submission.UserName = "Student1";
+            submission.ZippedDllFiles = Encoding.ASCII.GetBytes("demotext");
+            SubmissionEntity postEntity = await _uploadClient.PostSubmissionAsync(submission);
+
+            byte[] submissionFile = await _downloadClient.GetSubmissionByUserNameAndSessionIdAsync(submission.UserName, submission.SessionId);
+            string text = Encoding.ASCII.GetString(submissionFile);
+            await _downloadClient.DeleteAllSubmissionsAsync();
+            Assert.AreEqual(text, "demotext");
+
+        }
+        [TestMethod()]
+        public async Task PostAndGetTestAnalysis()
+        {
+            await _downloadClient.DeleteAllAnalysisAsync();
+            AnalysisData analysis = new AnalysisData();
+            analysis.SessionId = "1";
+            analysis.UserName = "Student1";
+            analysis.AnalysisFile = Encoding.ASCII.GetBytes("demotext");
+            AnalysisEntity postEntity = await _uploadClient.PostAnalysisAsync(analysis);
+            IReadOnlyList<AnalysisEntity> entities = await _downloadClient.GetAnalysisByUserNameAndSessionIdAsync(analysis.UserName, analysis.SessionId);
+            await _downloadClient.DeleteAllAnalysisAsync();
+            Assert.AreEqual(1, entities.Count);
+            Assert.AreEqual(entities[0].SessionId, postEntity.SessionId);
+            Assert.AreEqual(entities[0].UserName, postEntity.UserName);
+            string text = Encoding.ASCII.GetString(entities[0].AnalysisFile);
+            Assert.AreEqual("demotext", text);
+
+        }
+    }
+}
---
+++ UnitTests/UtilityTests.cs
@@ -0,0 +1,69 @@
+﻿using ServerlessFunc;
+using System.Collections;
+using System.Text;
+using System.Text.Json;
+
+namespace UnitTests
+{
+    [TestClass()]
+    public class UtilityTests
+    {
+        [TestMethod()]
+        public async Task BlobUtilityTest()
+        {
+            string blobName = "testblob";
+            byte[] blobcontent = Encoding.ASCII.GetBytes("demotext");
+            string containerName = "democontainer";
+            string connectionString = "UseDevelopmentStorage=true";
+            await BlobUtility.UploadSubmissionToBlob(blobName, blobcontent, connectionString, containerName);
+            byte[] getBlobContent = await BlobUtility.GetBlobContentAsync(containerName, blobName, connectionString);
+            await BlobUtility.DeleteContainer(containerName, connectionString);
+            CollectionAssert.AreEqual(blobcontent, getBlobContent);
+        }
+
+        [TestMethod()]
+
+        public void AnalysisResultEncodeDecodeTest()
+        {
+            AnalyzerResult result1 = new AnalyzerResult("100", 1, "Successful");
+            string jsonString1 = JsonSerializer.Serialize(result1);
+            byte[] analysisFile =  Encoding.UTF8.GetBytes(jsonString1);
+
+            string jsonString2 = Encoding.UTF8.GetString(analysisFile);
+            AnalyzerResult result2 = JsonSerializer.Deserialize<AnalyzerResult>(jsonString2);
+            Assert.AreEqual(result1.AnalyserID, result2.AnalyserID);
+            Assert.AreEqual(result1.Verdict, result2.Verdict);
+            Assert.AreEqual(result1.ErrorMessage, result2.ErrorMessage);
+        }
+
+        [TestMethod()]
+        public void InsightsUtilityTest1()
+        {
+            List<string> list = new List<string>
+            {
+                "text1",
+                "text2",
+                "text3"
+            };
+            byte[] byteArray = InsightsUtility.ListToByte(list);
+            List<string> deserializedList = InsightsUtility.ByteToList(byteArray);
+            Assert.AreEqual(3,deserializedList.Count);
+            for(int i = 0; i < deserializedList.Count; i++)
+            {
+                Assert.AreEqual(list[i], deserializedList[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsightsUtilityTest2()
+        {
+            Dictionary<string, List<AnalyzerResult>> analysis = InsightsTests.GetAnalysisResult(1, 0);
+            byte[] analysisResultBytes = InsightsUtility.ConvertDictionaryToAnalysisFile(analysis);
+            Dictionary<string, List<AnalyzerResult>> Deserializedanalysis = InsightsUtility.ConvertAnalysisFileToDictionary(analysisResultBytes);
+            Assert.AreEqual(analysis.Count, Deserializedanalysis.Count);
+            Assert.AreEqual(analysis["File1"][0].AnalyserID, Deserializedanalysis["File1"][0].AnalyserID);
+            Assert.AreEqual(analysis["File1"][0].Verdict, Deserializedanalysis["File1"][0].Verdict);
+            Assert.AreEqual(analysis["File1"][0].ErrorMessage, Deserializedanalysis["File1"][0].ErrorMessage);
+        }
+    }
+}
