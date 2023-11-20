e3c494feefcfa75ece50e7ce3fe33c9e19c1f23a
nideez45 <nideeshnagarajan@gmail.com>
2023-11-20 22:05:08 +0530

    added comments to entities

--- ServerlessFunc/SessionEntity.cs
+++ ServerlessFunc/SessionEntity.cs
@@ -6,10 +6,20 @@ using JsonIgnoreAttribute = System.Text.Json.Serialization.JsonIgnoreAttribute;
 
 namespace ServerlessFunc
 {
+    /// <summary>
+    /// Represents an entity for storing session information.
+    /// </summary>
     public class SessionEntity : ITableEntity
     {
+        /// <summary>
+        /// The partition key name for SessionEntity.
+        /// </summary>
         public const string PartitionKeyName = "SessionEntityPartitionKey";
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SessionEntity"/> class.
+        /// </summary>
+        /// <param name="sessionData">The data associated with the session.</param>
         public SessionEntity( SessionData sessionData = null )
         {
             PartitionKey = PartitionKeyName;
@@ -29,75 +39,109 @@ namespace ServerlessFunc
             ETag = new ETag();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SessionEntity"/> class.
+        /// </summary>
         public SessionEntity() : this( null ) { }
 
         /// <summary>
-        /// To store session Id.
+        /// To store the session Id.
         /// </summary>
         [JsonInclude]
         [JsonPropertyName( "SessionId" )]
         public string SessionId { get; set; }
 
-
         /// <summary>
-        /// To store Unique id for the entry.
+        /// To store the unique identifier for the entry.
         /// </summary>
         [JsonInclude]
         [JsonPropertyName( "Id" )]
         public string Id { get; set; }
 
-
         /// <summary>
-        /// To store the host user name.
+        /// To store the host username.
         /// </summary>
         [JsonInclude]
         [JsonPropertyName( "HostUserName" )]
         public string HostUserName { get; set; }
 
+        /// <summary>
+        /// To store the partition key.
+        /// </summary>
         [JsonInclude]
         [JsonPropertyName( "PartitionKey" )]
         public string PartitionKey { get; set; }
 
+        /// <summary>
+        /// To store the row key.
+        /// </summary>
         [JsonInclude]
         [JsonPropertyName( "RowKey" )]
         public string RowKey { get; set; }
 
         /// <summary>
-        /// To store start Timestamp of the session.
+        /// To store the start timestamp of the session.
         /// </summary>
         [JsonInclude]
         [JsonPropertyName( "Timestamp" )]
         public DateTimeOffset? Timestamp { get; set; }
 
         /// <summary>
-        /// To store the tests done in session
+        /// To store the tests conducted in the session.
         /// </summary>
         [JsonInclude]
         [JsonPropertyName( "Tests" )]
         public byte[] Tests { get; set; }
 
         /// <summary>
-        /// To store the list of studnets joined in session
+        /// To store the list of students joined in the session.
         /// </summary>
         [JsonInclude]
         [JsonPropertyName( "Students" )]
         public byte[] Students { get; set; }
 
+        /// <summary>
+        /// To store the mapping of test names to IDs.
+        /// </summary>
         [JsonInclude]
         [JsonPropertyName( "TestNameToID" )]
         public byte[] TestNameToID { get; set; }
 
+        /// <summary>
+        /// The ETag for concurrency control.
+        /// </summary>
         [JsonIgnore]
         public ETag ETag { get; set; }
     }
 
+    /// <summary>
+    /// Represents the data associated with a session.
+    /// </summary>
     public class SessionData
     {
+        /// <summary>
+        /// The host username for the session.
+        /// </summary>
         public string HostUserName { get; set; }
+
+        /// <summary>
+        /// The session ID.
+        /// </summary>
         public string SessionId { get; set; }
+
+        /// <summary>
+        /// The byte array representing the tests conducted in the session.
+        /// </summary>
         public byte[] Tests { get; set; }
+
+        /// <summary>
+        /// The byte array representing the list of students joined in the session.
+        /// </summary>
         public byte[] Students { get; set; }
+
+        /// <summary>
+        /// The byte array representing the mapping of test names to IDs.
+        /// </summary>
         public byte[] TestNameToID { get; set; }
     }
 }
-
--- ServerlessFunc/SubmissionEntity.cs
+++ ServerlessFunc/SubmissionEntity.cs
@@ -5,13 +5,23 @@ using ITableEntity = Azure.Data.Tables.ITableEntity;
 
 namespace ServerlessFunc
 {
+    /// <summary>
+    /// Represents an entity for storing submission information.
+    /// </summary>
     public class SubmissionEntity : ITableEntity
     {
+        /// <summary>
+        /// The partition key name for SubmissionEntity.
+        /// </summary>
         public const string PartitionKeyName = "SubmissionEntityPartitionKey";
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubmissionEntity"/> class.
+        /// </summary>
+        /// <param name="sessionId">The session ID of the submission.</param>
+        /// <param name="username">The username of the user who submitted the file.</param>
         public SubmissionEntity( string sessionId , string username )
         {
-            //structure of the submission entitiy. 
             PartitionKey = PartitionKeyName;
             RowKey = Guid.NewGuid().ToString();
             Id = RowKey;
@@ -21,58 +31,85 @@ namespace ServerlessFunc
             Timestamp = DateTime.Now;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubmissionEntity"/> class.
+        /// </summary>
         public SubmissionEntity() : this( null , null ) { }
 
         /// <summary>
-        /// To store the session id of the submission.
+        /// The session ID of the submission.
         /// </summary>
         [JsonInclude]
-        [JsonPropertyName( "SessionId" )] //Unique id for the session conducted
+        [JsonPropertyName( "SessionId" )]
         public string SessionId { get; set; }
 
         /// <summary>
-        /// To store the blob name of the submission.
+        /// The blob name associated with the submission.
         /// </summary>
         [JsonInclude]
         [JsonPropertyName( "BlobName" )]
         public string BlobName { get; set; }
 
-
+        /// <summary>
+        /// The unique identifier for the SubmissionEntity.
+        /// </summary>
         [JsonInclude]
-        [JsonPropertyName( "Id" )] //unique id for storing the primary key. 
+        [JsonPropertyName( "Id" )]
         public string Id { get; set; }
 
         /// <summary>
-        /// To store the username of the submitted person.
+        /// The username of the user who submitted the file.
         /// </summary>
         [JsonInclude]
-        [JsonPropertyName( "UserName" )] //Username of the user who submitted file.
+        [JsonPropertyName( "UserName" )]
         public string UserName { get; set; }
 
+        /// <summary>
+        /// The partition key for storage.
+        /// </summary>
         [JsonInclude]
         [JsonPropertyName( "PartitionKey" )]
         public string PartitionKey { get; set; }
 
+        /// <summary>
+        /// The row key for storage.
+        /// </summary>
         [JsonInclude]
         [JsonPropertyName( "RowKey" )]
         public string RowKey { get; set; }
 
         /// <summary>
-        /// To store the Timestamp of the submission.
+        /// The timestamp of the submission.
         /// </summary>
         [JsonInclude]
-        [JsonPropertyName( "Timestamp" )]// time stamp when submission occured. 
+        [JsonPropertyName( "Timestamp" )]
         public DateTimeOffset? Timestamp { get; set; }
 
+        /// <summary>
+        /// The ETag for concurrency control.
+        /// </summary>
         [JsonIgnore]
         public ETag ETag { get; set; }
     }
 
+    /// <summary>
+    /// Represents the data associated with a submission.
+    /// </summary>
     public class SubmissionData
     {
+        /// <summary>
+        /// The session ID for the submission.
+        /// </summary>
         public string SessionId { get; set; }
+
+        /// <summary>
+        /// The username of the user who submitted the file.
+        /// </summary>
         public string UserName { get; set; }
+
+        /// <summary>
+        /// The zipped DLL files for the submission.
+        /// </summary>
         public byte[] ZippedDllFiles { get; set; }
     }
-
 }
