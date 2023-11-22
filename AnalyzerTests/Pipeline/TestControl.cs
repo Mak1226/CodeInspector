80589997eaac71a10631a350546f0695c2789b6f
aruns <aruns@DESKTOP-H05BMP2>
2023-11-22 18:43:24 +0530

    controlflow is left

--- AnalyzerTests/Pipeline/TestControl.cs
+++ AnalyzerTests/Pipeline/TestControl.cs
@@ -6,18 +6,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Analyzer.Parsing;
+using System.Reflection;
 
 namespace Analyzer.Pipeline.Tests
 {
     [TestClass()]
     public class TestControl
     {
+        public class TestClassForControl
+        {
+            public void MethodWithUselessControlFlow()
+            {
+                int x = 10;
+                int y = 20;
+
+                if (x > y)
+                {
+                    Console.WriteLine( "x is greater than y" );
+                }
+                else
+                {
+                    Console.WriteLine( "x is not greater than y" );
+                }
+
+                // This is a useless jump followed by a no-op
+                if (x < y)
+                {
+                    Console.WriteLine( "x is less than y" );
+                }
+                else
+                {
+                    Console.WriteLine( "x is not less than y" );
+                    // No-op
+                    Nop();
+                }
+
+                Console.WriteLine( "Method completed" );
+            }
+
+            private static void Nop()
+            {
+                // No-op
+            }
+        }
+
         [TestMethod()]
         public void Test1()
         {
             // Specify the path to the DLL file
-            string path = "..\\..\\..\\..\\Analyzer\\TestDLLs\\controlflow.dll";
-
+            //string path = "..\\..\\..\\..\\Analyzer\\TestDLLs\\controlflow.dll";
+            string path = Assembly.GetExecutingAssembly().Location;
             // Create a list of DLL paths
             ParsedDLLFile dllFile = new(path);
 
