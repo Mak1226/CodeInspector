8a88a7b97e17fe6253d09ba1804b819b1c54257e
Mangesh Dalvi <dalvimangesh000@gmail.com>
2023-11-22 17:24:11 +0530

    Minor

--- AnalyzerTests/AnalyzerResultTests.cs
+++ AnalyzerTests/AnalyzerResultTests.cs
@@ -5,10 +5,32 @@ namespace Analyzer.Tests
     [TestClass()]
     public class AnalyzerResultTests
     {
-        [TestMethod()]
-        public void AnalyzerResultTest()
+        [TestMethod]
+        public void TestEquality()
         {
-            Assert.Fail();
+            // Arrange
+            var result1 = new AnalyzerResult("ID1", 1, "Error 1");
+            var result2 = new AnalyzerResult("ID1", 1, "Error 1");
+            var result3 = new AnalyzerResult("ID2", 2, "Error 2");
+
+            // Act & Assert
+            Assert.AreEqual(result1, result2);
+            Assert.AreNotEqual(result1, result3);
+            Assert.AreNotEqual(result2, result3);
+        }
+
+        [TestMethod]
+        public void TestInequality()
+        {
+            // Arrange
+            var result1 = new AnalyzerResult("ID1", 1, "Error 1");
+            var result2 = new AnalyzerResult("ID1", 1, "Error 1");
+            var result3 = new AnalyzerResult("ID2", 2, "Error 2");
+
+            // Act & Assert
+            Assert.IsFalse(result1 != result2);
+            Assert.IsTrue(result1 != result3);
+            Assert.IsTrue(result2 != result3);
         }
     }
 }
--- AnalyzerTests/AnalyzerTests.cs
+++ AnalyzerTests/AnalyzerTests.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
 
 namespace Analyzer.Tests
 {
@@ -34,24 +36,48 @@ namespace Analyzer.Tests
 
             List<string> paths = new()
             {
-                "..\\..\\..\\TestDLLs\\CasingChecker.dll",
+                "..\\..\\..\\TestDLLs\\Abstract.dll"
             };
 
             analyzer.LoadDLLFileOfStudent(paths);
 
             Dictionary<string, List<AnalyzerResult>> result = analyzer.Run();
 
-           foreach(KeyValuePair<string , List<AnalyzerResult>> dll in result)
-           {
+            Dictionary<string, List<AnalyzerResult>> original = new();
+
+            original["Abstract.dll"] = new List<AnalyzerResult> { 
+
+                new AnalyzerResult("101", 1, ""),
+                new AnalyzerResult("102", 0, "Classes ClassLibrary1.Badname contains only static fields and methods, but has non -static, visible constructor.Try changing it to private or make it static."),
+                new AnalyzerResult("103", 1, "No violation found."),
+                new AnalyzerResult("104", 1, "No violation found."),
+                new AnalyzerResult("105", 1, "Depth of inheritance rule followed by all classes."),
+                new AnalyzerResult("106", 1, "Everything looks fine. No readonly array fields found."),
+                new AnalyzerResult("107", 1, "Everything looks fine.No switch statements found."),
+                new AnalyzerResult("108", 0, "No violations found."),
+                new AnalyzerResult("109", 0, "No unused local variables found."),
+                new AnalyzerResult("110", 0, "No occurrences of useless control flow found."),
+                new AnalyzerResult("111", 0, "No Violation FoundIncorrect Abstract Class Naming : Badname"),
+                new AnalyzerResult("112", 0, "No Violation FoundIncorrect Method Naming : .ctor"),
+                new AnalyzerResult("113", 1, "No methods have cyclomatic complexity greater than 10"),
+                new AnalyzerResult("114", 1, ""),
+                new AnalyzerResult("115", 1, "No Violation Found"),
+                new AnalyzerResult("116", 1, ""),
+                new AnalyzerResult("117", 1, "No Violation Found")
+
+            };
+
+            foreach(KeyValuePair<string , List<AnalyzerResult>> dll in result)
+            {
                 Console.WriteLine(dll.Key);
 
                 foreach(AnalyzerResult res in dll.Value)
                 {
                     Console.WriteLine(res.AnalyserID + " " + res.Verdict + " " + res.ErrorMessage);
                 }
-           }
+            }
 
-            Assert.Fail();
+            Assert.AreEqual(result.ToString(), original.ToString());
         }
     }
 }
