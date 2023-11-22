7db65326a690d9c77066fe8c9047970b2e60a1f3
lechurnair <75621929+lechurnair@users.noreply.github.com>
2023-11-23 01:47:53 +0530

    ContentServer Unit tests updated

--- ContentUnitTesting/ContentClientServerTest/ContentServerTest.cs
+++ ContentUnitTesting/ContentClientServerTest/ContentServerTest.cs
@@ -55,9 +55,9 @@ namespace ContentUnitTesting.ContentClientServerTest
             Directory.CreateDirectory(tempDirectory);
             File.WriteAllText(Path.Combine(tempDirectory, "TestDll1.dll"), "DLL Content 1");
             File.WriteAllText(Path.Combine(tempDirectory, "TestDll2.dll"), "DLL Content 2");
-            string encoding = fileHandler.HandleUpload(tempDirectory, "testSessionID");
+            string encoding = fileHandler.HandleUpload(tempDirectory, "testSessionID2");
 
-            contentServer.HandleRecieve(encoding, "testClientID2");
+            contentServer.HandleRecieve(encoding, "testClientID");
             Assert.IsFalse(contentServer.analyzerResult.ContainsKey("File1"));
             Directory.Delete(tempDirectory, true);
         }
@@ -80,6 +80,45 @@ namespace ContentUnitTesting.ContentClientServerTest
             Assert.IsTrue(contentServer.analyzerResult.ContainsKey("File1"));
             Directory.Delete(tempDirectory, true);
         }
+        /// <summary>
+        /// Test to check if proper configurations are being stored
+        /// </summary>
+        [TestMethod]
+        public void ConfigureTest()
+        {
+            ContentServer contentServer = new ContentServer(_communicator, _analyzer);
+            IDictionary<int, bool> configuration = new Dictionary<int, bool>
+            {
+                { 1, true },
+                { 2, false },
+                { 3, true },
+            };
+            contentServer.Configure(configuration);
+            Assert.AreEqual(configuration, _analyzer.GetTeacherOptions());
+        }
+        [TestMethod]
+        public void EmptySessionIDTest()
+        {
+            ContentServer contentServer = new ContentServer(_communicator, _analyzer);
+            contentServer.SetSessionID("");
+            Assert.IsTrue(contentServer.analyzerResult.Count() == 0);
+        }
 
+        [TestMethod]
+        public void PreviousSessionIDTest()
+        {
+            ContentServer contentServer = new ContentServer(_communicator, _analyzer);
+            contentServer.SetSessionID("TestSessionID");
+            IFileHandler fileHandler = new FileHandler();
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+            File.WriteAllText(Path.Combine(tempDirectory, "TestDll1.dll"), "DLL Content 1");
+            string encoding = fileHandler.HandleUpload(tempDirectory, "TestSessionID");
+
+            contentServer.HandleRecieve(encoding, "testClientID");
+            contentServer.SetSessionID("TestSessionID");
+            Assert.IsFalse(contentServer.analyzerResult.Count == 0);
+            Directory.Delete(tempDirectory, true);
+        }
     }
 }
