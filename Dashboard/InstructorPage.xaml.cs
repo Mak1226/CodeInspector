e3d8c12543584cb3b2b73f4db132f5bc0781ebb6
saarang <saarangs2002@gmail.com>
2023-11-23 23:44:56 +0530

    comments added

--- Dashboard/InstructorPage.xaml.cs
+++ Dashboard/InstructorPage.xaml.cs
@@ -1,4 +1,15 @@
-﻿using ContentPage;
+﻿/******************************************************************************
+ * Filename    = InstructorPage.xaml.cs
+ *
+ * Author      = Saarang S
+ *
+ * Product     = Analyzer
+ * 
+ * Project     = Dashboard
+ *
+ * Description = Code behind of a Instructor Page.
+ *****************************************************************************/
+using ContentPage;
 using SessionState;
 using System;
 using System.Collections.Generic;
@@ -16,45 +27,64 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ViewModel;
+using ServerlessFuncUI;
+
 
 namespace Dashboard
 {
     /// <summary>
-    /// Interaction logic for InstructorPage.xaml
+    /// Represents the WPF page for the instructor's dashboard.
     /// </summary>
     public partial class InstructorPage : Page
     {
         private readonly ServerPage _contentServerPage;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InstructorPage"/> class.
+        /// </summary>
+        /// <param name="userName">The username of the instructor.</param>
+        /// <param name="userId">The user ID of the instructor.</param>
         public InstructorPage( string userName , string userId )
         {
             InitializeComponent();
             try
             {
+                // Create and set up the ViewModel
                 InstructorViewModel viewModel = new(userName,userId);
                 DataContext = viewModel;
 
+                // Create and set up the ServerPage
                 _contentServerPage = new ServerPage ( viewModel.Communicator, userId);
                 ResultFrame.Content = _contentServerPage;
             }
             catch (Exception exception)
             {
-                // If an exception occurs during ViewModel creation, show an error message and shutdown the application.
+                // Display an error message and shutdown the application on exception
                 _ = MessageBox.Show(exception.Message);
                 Application.Current.Shutdown();
             }
         }
 
+        /// <summary>
+        /// Event handler for when the page is unloaded.
+        /// </summary>
         private void InstructorPage_Unloaded( object sender , RoutedEventArgs e )
         {
             
         }
 
+        /// <summary>
+        /// Event handler for the logout button click.
+        /// </summary>
         private void LogoutButton_Click(object sender, RoutedEventArgs e)
         {
-            // If a valid NavigationService exists, navigate to the "Login.xaml" page.
+            // Navigate to the AuthenticationPage when the logout button is clicked
             NavigationService?.Navigate( new Uri( "AuthenticationPage.xaml" , UriKind.Relative ) );
         }
 
+        /// <summary>
+        /// Event handler for when a student is selected in the list.
+        /// </summary>
         private void Student_Selected(object sender, MouseButtonEventArgs e)
         {
             if (sender is ListViewItem item && item.IsSelected)
@@ -63,6 +93,7 @@ namespace Dashboard
                 {
                     if (clickedStudent.Id != null)
                     {
+                        // Set the session ID in the ServerPage when a student is selected
                         _contentServerPage.SetSessionID( clickedStudent.Id );
                     }
                 }
--- ViewModel/InstructorViewModel.cs
+++ ViewModel/InstructorViewModel.cs
@@ -29,16 +29,22 @@ using System.Windows;
 
 namespace ViewModel
 {
+    /// <summary>
+    /// Represents the ViewModel for the instructor, providing data and functionality for the instructor's view.
+    /// </summary>
     public class InstructorViewModel : INotifyPropertyChanged, IEventHandler
     {
-        private readonly ISessionState _studentSessionState; // To manage the connected students
+        private readonly ISessionState _studentSessionState;
 
         /// <summary>
-        /// Constructor for the DashboardViewModel.
+        /// Initializes a new instance of the <see cref="InstructorViewModel"/> class.
         /// </summary>
-        public InstructorViewModel( string userName, string userId, ICommunicator? communicator = null )
+        /// <param name="name">The Name of the instructor.</param>
+        /// <param name="userId">The user ID of the instructor.</param>
+        /// <param name="communicator">An optional communicator, default is set to the server communicator.</param>
+        public InstructorViewModel( string name, string userId, ICommunicator? communicator = null )
         {
-            UserName = userName;
+            Name = name;
             UserId = userId;
             _studentSessionState = new StudentSessionState();
             Communicator = communicator ?? CommunicationFactory.GetServer();
@@ -46,90 +52,98 @@ namespace ViewModel
             string ipPort = Communicator.Start(null, null, "server", "Dashboard");
             Communicator.Subscribe(this, "Dashboard");
             string[] parts = ipPort.Split(':');
-            try
+            if(parts.Length==2)
             {
                 IpAddress = parts[0];
                 ReceivePort = parts[1];
                 OnPropertyChanged(nameof(IpAddress));
                 OnPropertyChanged(nameof(ReceivePort));
             }
-            catch { }
+            else
+            {
+                throw new Exception( "Invalid Port/Ip returned by communicator" );
+            }
         }
+
         /// <summary>
-        /// Converting student list from <typeparamref name="List"/> to <typeparamref name="ObservableCollection"/>
+        /// Gets the list of students in the session.
         /// </summary>
         public ObservableCollection<Student> StudentList => new( _studentSessionState.GetAllStudents() );
 
+        /// <summary>
+        /// Gets the count of students in the session.
+        /// </summary>
         public int StudentCount => _studentSessionState.GetStudentsCount();
 
+        /// <summary>
+        /// Gets the communicator used for communication.
+        /// </summary>
         public ICommunicator Communicator { get; }
 
-        public string UserName { get; init; }
+        /// <summary>
+        /// Gets the Name of the instructor.
+        /// </summary>
+        public string Name { get; init; }
 
+        /// <summary>
+        /// Gets the user ID of the instructor.
+        /// </summary>
         public string UserId { get; init; }
 
+        /// <summary>
+        /// Gets the receiving port for the instructor.
+        /// </summary>
         public string? ReceivePort { get; private set; }
 
+        /// <summary>
+        /// Gets the IP address for the instructor.
+        /// </summary>
         public string? IpAddress { get; private set; }
 
+        /// <summary>
+        /// Event triggered when a property of the ViewModel changes.
+        /// </summary>
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        /// <summary>
+        /// Handles the PropertyChanged event for the ViewModel.
+        /// </summary>
+        /// <param name="property">The name of the changed property.</param>
         private void OnPropertyChanged(string property)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
         }
 
+
         private static Dispatcher Dispatcher => Application.Current?.Dispatcher ?? Dispatcher.CurrentDispatcher;
-        private string? GetPrivateIp()
-        {
-            string hostName = Dns.GetHostName();
-            IPAddress[] addresses = Dns.GetHostAddresses(hostName);
 
-            foreach (IPAddress address in addresses)
-            {
-                if (address.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    if (address.ToString().Length >= 3 && address.ToString().Substring(0, 3) == "10.")
-                    {
-                        return address.ToString();
-                    }
-                }
-            }
-            return null;
-        }
-
-        private static string SerializeStudnetInfo(string? name, string? rollNo, string? ip, string? port)
-        {
-            return $"{rollNo}|{name}|{ip}|{port}";
-        }
-
+        /// <summary>
+        /// Deserializes student information from a serialized string.
+        /// </summary>
+        /// <param name="data">The serialized student information.</param>
+        /// <returns>A tuple containing student information.</returns>
         private static (string?, string?, string?, int, int) DeserializeStudnetInfo(string data)
         {
             string[] parts = data.Split('|');
             if (parts.Length == 5)
             {
-                try
-                {
-                    return
-                    (
-                        parts[0],
-                        parts[1],
-                        parts[2],
-                        int.Parse(parts[3]),
-                        int.Parse(parts[4])
-                    );
-                }
-                catch { }
-
+                return
+                (
+                    parts[0],
+                    parts[1],
+                    parts[2],
+                    int.Parse(parts[3]),
+                    int.Parse(parts[4])
+                );
             }
             return (null, null, null, 0, 0);
         }
 
         /// <summary>
-        /// Adds or removes a student from the session based on received serialized student data.
+        /// Adds a student based on the received serialized student information.
         /// </summary>
-        /// <param name="serializedStudent">Serialized student data to process.</param>
-        /// <returns>True if the operation was successful, false otherwise.</returns>
+        /// <param name="serializedStudnet">The serialized student information.</param>
+        /// <returns>True if the student is successfully added, false otherwise.</returns>
         private bool AddStudnet(string serializedStudnet)
         {
             Trace.WriteLine($"One message received {serializedStudnet}");
@@ -147,15 +161,14 @@ namespace ViewModel
                     {
                         _studentSessionState.AddStudent(rollNo, name, ip, port);
                         Communicator.Send("1", $"{rollNo}");
-                        Trace.WriteLine($"Added student: Roll No - {rollNo}, Name - {name}, IP - {ip}, Port - {port}");
+                        Trace.WriteLine($"[Instructor View Model] Added student: Roll No - {rollNo}, Name - {name}, IP - {ip}, Port - {port}");
                     }
                     else if (isConnect == 0)
                     {
                         _studentSessionState.RemoveStudent(rollNo);
                         Communicator.Send("0", $"{rollNo}");
-                        Trace.WriteLine($"Removed student: Roll No - {rollNo}");
+                        Trace.WriteLine($"[Instructor View Model] Removed student: Roll No - {rollNo}");
                     }
-
                     OnPropertyChanged(nameof(StudentList));
                    
                     OnPropertyChanged(nameof(StudentCount));
@@ -164,12 +177,16 @@ namespace ViewModel
             }
             return false;
         }
+
+        /// <summary>
+        /// Handles the reception of a network message.
+        /// </summary>
+        /// <param name="data">The received network message.</param>
+        /// <returns>An empty string.</returns>
         public string HandleMessageRecv(Networking.Models.Message data)
         {
-            Dispatcher.Invoke((string data) =>
-            {
-                AddStudnet(data);
-            }, data.Data);
+            Trace.WriteLine( $"[Instructor View Model] Received message {data.Data}" );
+            AddStudnet(data.Data);
             return "";
         }
     }
