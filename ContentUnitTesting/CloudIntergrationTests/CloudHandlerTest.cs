6bc5bbc62595e73f940899e274403eb42192998c
padath314 <112001057@smail.iitpkd.ac.in>
2023-11-24 01:16:07 +0530

    Added cloud handler test

---
+++ ContentUnitTesting/CloudIntergrationTests/CloudHandlerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentUnitTesting.CloudIntergrationTests
+{
+    [TestClass]
+    public class CloudHandlerTest
+    {
+        private CloudHandler _cloudHandler;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // You can mock the dependencies or use actual implementations based on your testing strategy
+            _cloudHandler = new CloudHandler();
+        }
+
+        [TestMethod]
+        public void CloudHandler_Initialization_Success()
+        {
+            // Arrange
+            CloudHandler cloudHandler;
+
+            // Act
+            cloudHandler = new CloudHandler();
+
+            // Assert
+            Assert.IsNotNull( cloudHandler );
+            Assert.IsNotNull( cloudHandler.SessionID );
+            Assert.IsFalse( string.IsNullOrEmpty( cloudHandler.SessionID ) );
+        }
+    }
+}
