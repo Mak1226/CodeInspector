712e53c6076ef8dfd9fd0a7c401d78032b8665ba
sreelux <112001042@smail.iitpkd.ac.in>
2023-11-24 00:14:41 +0530

    serverReceiveHandlerTest

---
+++ ContentUnitTesting/ModelTest/ServerReceiveHandlerTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Content.Model;
+using Networking.Models;
+using Content.Encoder;
+using Analyzer;
+using Newtonsoft.Json.Linq;
+using Networking.Communicator;
+using ContentUnitTesting.ContentClientServerTest;
+
+namespace ContentUnitTesting.ModelTest
+{
+    [TestClass]
+    public class ServerReceiveHandlerTest
+    {
+   
+        [TestMethod]
+        public void HandleMessageRecv_ValidMessage()
+
+        {
+
+            MockCommunicator mockCommunicator = new();
+            MockAnalyzer mockAnalyzer = new();
+            ContentServer server = new( mockCommunicator , mockAnalyzer , "sessionID" );
+            ServerRecieveHandler handler = new( server );
+            Dictionary<string , List<AnalyzerResult>> analyzerResult = new()
+             {
+                 { "File1", new List<AnalyzerResult> { new AnalyzerResult("Analyzer1", 1, "No errors") } },
+             };
+            AnalyzerResultSerializer serializer = new();
+
+            string encoding = serializer.Serialize( analyzerResult );
+            Assert.IsTrue( analyzerResult.ContainsKey( "File1" ) );
+            var message = new Message { Data = encoding, SenderID = "senderID" }; // Create a message instance with some data
+
+            // Act
+            handler.HandleMessageRecv( message );
+            Assert.IsTrue( analyzerResult.ContainsKey( "File1" ) );
+
+            // Assert
+            // Assuming analyzerResult is a public property 
+            Assert.IsNotNull( server.analyzerResult ); // Assert that analyzerResult is not null
+
+        }
+    }
+}
