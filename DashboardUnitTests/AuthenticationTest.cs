bbd6005f5fbb36d9c10a431b8774256acec41e41
sushmakamuju <131590836+sushmakamuju@users.noreply.github.com>
2023-11-24 15:52:41 +0530

    authentication unittest (#37)

---
+++ DashboardUnitTests/AuthenticationTest.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Dashboard.Authentication;
+
+using Dashboard;
+
+
+namespace DashboardViewModelUnitTests
+{
+    [TestClass]
+    public class AuthenticationTest
+    {
+
+        //    }
+        //}
+        [TestMethod]
+        public async Task AuthenticationViewModelUnitTest()
+        {
+            AuthenticationResult result = await Authenticator.Authenticate();
+            Assert.IsNotNull( result );
+            if (result.IsAuthenticated == false) { Assert.Fail( "Authentication failed " ); }
+            else
+            { Console.WriteLine( "Authentication done" ); }
+
+
+        }
+        //[Fact]
+        //     Giving a timeout of 50 ms to reduce the wait time
+        //  var returnval = await _viewModel.AuthenticateUser( 50 );
+        // Assert
+        //ssert.Equals( "false" , returnval[0] );
+
+    }
+}
